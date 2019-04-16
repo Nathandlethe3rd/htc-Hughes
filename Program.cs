7cdcd71ddc8cdf5d09ba477b6d2bcfe60fb2a678
User <User@DEV-022829.OnBase.net>
2019-04-16 19:27:51 -0400

    oof

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace Htc_Hughes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
+          
+            Console.WriteLine("Bobby did a bad, you must help him.");
+            Console.WriteLine("One day Bobby was playing Super Smash Bros. Ultimate while listening to Fortntie raps and using his main, Marth");
+            Console.WriteLine("He palyed online and saw someone using Little Mac, he said");
+            Console.WriteLine("Who uses Little Mac, he sucks");
+            Console.WriteLine("Unbeknownst to Bobby this player did not suck");
+            Console.WriteLine("Bobby lost HORRIBLY, he immideatly raged, he couldn't believe he had lost to a Little Mac");
+            Console.WriteLine("What should Bobby do? /n Play Zelda or Report the Player");
+            string Ans1 = Console.ReadLine();
+            if (Ans1 == "Play Zelda"){
+                Console.WriteLine("Bobby boots up Zelda: Breath of The Wild and redeems his daily amiibo benefts, boy those $220.33 were sure spent well!");
+                Console.WriteLine("");
+            }
+            
+
         }
     }
 }
